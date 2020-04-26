073c44fc34207fe6d03f5d96f8b2b161174d01ff
Batanai <31771294+Batanai@users.noreply.github.com>
2020-04-26 23:14:52 +0200

    End of section 5

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -9,6 +9,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [ApiExplorerSettings(IgnoreApi = true)] //swagger to ignore endpoint
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
