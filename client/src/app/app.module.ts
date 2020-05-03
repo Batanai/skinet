2ee656d34026d8d54c56cf6f67147685b4a09df7
Batanai <31771294+Batanai@users.noreply.github.com>
2020-05-03 23:09:35 +0200

    End of section 12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,8 +8,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { from } from 'rxjs';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { ErrorInterceptor } from './core/Interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/Interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,10 +22,12 @@ import { ErrorInterceptor } from './core/Interceptors/error.interceptor';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { ShopService } from './../shop.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/products';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'product-details',
@@ -11,7 +12,12 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopSevice: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopSevice: ShopService,
+              private activatedRoute: ActivatedRoute,
+              private bcService: BreadcrumbService) {
+
+                this.bcService.set('@productDetails', '');
+              }
 
   ngOnInit() {
     this.loadProduct();
@@ -20,6 +26,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     this.shopSevice.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     });
